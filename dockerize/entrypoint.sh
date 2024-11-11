1d33353bedf7a8d42936f50b6293f4f99b2e9a72
Marcelo F Andrade <mfandrade@gmail.com>
2024-11-11 12:33:06 -0300

    docs(docker): a comment in entrypoint

--- dockerize/entrypoint.sh
+++ dockerize/entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 if [ -z "$MARIADB_PASSWORD" ]; then
-  echo "ERROR: MARIADB_PASSWORD not defined" >/dev/stderr
-  exit 1
+    echo "ERROR: MARIADB_PASSWORD not defined" >/dev/stderr
+    exit 1
 fi
 CONF=/srv/app/server.conf
 
 PASS=$MARIADB_PASSWORD
-PASS_=$(printf '%s\n' "$PASS" | sed 's/:/%3A/g; s/\//%2F/g')
+PASS_=$(printf '%s\n' "$PASS" | sed 's/:/%3A/g; s/\//%2F/g') # URL encode
 echo "dbuser:$PASS_@tcp(backend:3306)/db" >"$CONF"
 
 EXEC="${1:-/bin/sh}"
 
 if [ ! -x "$EXEC" ]; then
-  echo "ERROR: $EXEC binary not found" >/dev/stderr
-  exit 2
+    echo "ERROR: $EXEC binary not found" >/dev/stderr
+    exit 2
 fi
 
 "$EXEC"
