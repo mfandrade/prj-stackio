6ef6df8c178695b784439f1e307ee043d3531dcb
Marcelo F Andrade <mfandrade@gmail.com>
2024-11-12 13:47:28 -0300

    feat(tf): stackio-backend resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,3 +27,68 @@ resource "kubernetes_secret" "credentials" {
   }
 }
 
+resource "kubernetes_stateful_set" "stackio_backend" {
+  metadata {
+    name      = "stackio-backend"
+    namespace = kubernetes_namespace.stackio.metadata[0].name
+    labels = {
+      app = "backend"
+    }
+  }
+
+  spec {
+    service_name = "backend"
+    replicas     = 1
+
+    selector {
+      match_labels = {
+        app = "backend"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "backend"
+        }
+      }
+
+      spec {
+        container {
+          name              = "stackio-backend"
+          image             = "mfandrade/stackio-backend:latest"
+          image_pull_policy = "Always"
+
+          port {
+            container_port = 3306
+          }
+
+          env {
+            name = "MARIADB_PASSWORD"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.credentials.metadata[0].name
+                key  = "MARIADB_PASSWORD"
+              }
+            }
+          }
+
+          volume_mount {
+            name       = "credentials-volume"
+            mount_path = "/mnt/credentials-volume"
+            read_only  = true
+          }
+        }
+
+        volume {
+          name = "credentials-volume"
+
+          secret {
+            secret_name = kubernetes_secret.credentials.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+}
+
