69fb6160918062550082e4a015656fdd62b3beb0
Marcelo F Andrade <mfandrade@gmail.com>
2024-11-12 13:44:37 -0300

    feat(tf): namespace resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,19 @@
+terraform {
+  required_version = ">= 1.1"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
+  }
+}
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "minikube"
+}
+
+resource "kubernetes_namespace" "stackio" {
+  metadata {
+    name = "stackio"
+  }
 }
